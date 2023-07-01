332ea35915d18146d8835c472afa3046230b181c
Likithasravani28 <137381225+Likithasravani28@users.noreply.github.com>
2023-07-01 10:07:28 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,37 @@
+public class Datatypes {
+    //PRIMITIVE DATA TYPES
+    public static void main(String args[]) 
+    {
+        //BYTE:	1 byte
+        //Stores whole numbers from -128 to 127
+        byte h=127;
+
+        //SHORT: 
+        //short	2 bytes	Stores whole numbers from -32,768 to 32,767
+        short s=32_767;
+
+        //INT: 4 bytes	
+        //Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        int a=2_147_483_647;
+
+        //FLOAT: 4 bytes	
+        //Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        float b=327789075789975689655555555555555555078.4367574456578767978687666666666666645634565769877777778666666666f;
+        
+        //DOUBLE: 8 bytes	
+        //Stores fractional numbers. Sufficient for storing 15 decimal digits
+        double c=42324673897098908098799878987998987789887869888888888888888888999999977777777777777798887777777777779999999997777899999999999999999999999999999999977777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777999999992.0990787;
+ 
+       //LONG: 
+       //l8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        long d=9_223_372_036_854_775_807l;
+
+        //BOOLEAN: 1 bit
+        //Stores true or false values
+        boolean e=true;
+
+        //CHAR: 
+        //char	2 bytes	Stores a single character/letter or ASCII values
+        char o='k';
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,13 @@
+class Variable
+{
+    public static void main(String args[])
+    {
+        int x=2;
+        int t=2+4;
+        String s="likki";
+        System.out.println(x);
+        System.out.println(s);
+        System.out.println(t);
+    }
+
+}
